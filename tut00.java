3d5e75d1f95efe87866dac32852655a0e228fd88
ccheung2 <rayccc@gmail.com>
2016-09-09 20:06:00 +0800

    Create tut00.java

---
+++ tut00.java
@@ -0,0 +1,8 @@
+public class tut00 {
+    public static void main(String[] args) {
+        String value = JOptionPane.showInputDialog("Enter something");
+        double halfvalue = Double.parseDouble(value)/2;
+        System.out.printf("Halve the value: %.2f\n", halfValue);
+                
+    }
+}
